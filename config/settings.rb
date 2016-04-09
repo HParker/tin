994fc5d6e5fd3ca6f4b16667e94d0d239ef75e14
Adam Hess <adamhess1991@gmail.com>
2016-04-09 11:24:49 -0700

    revert redis junk

--- config/settings.rb
+++ config/settings.rb
@@ -4,7 +4,7 @@ Envoker.load
 
 module Settings
   RACK_ENV       = Envoker.fetch("RACK_ENV", "development")
-  # REDIS_URL      = Envoker.fetch("REDIS_URL")
+  REDIS_URL      = Envoker.fetch("REDIS_URL")
   SESSION_KEY    = Envoker.fetch("SESSION_KEY")
   SESSION_SECRET = Envoker.fetch("SESSION_SECRET")
   SMTP_URL       = Envoker.fetch("SMTP_URL")
